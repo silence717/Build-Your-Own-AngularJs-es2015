cc3ece918bdc7a306193f2717e301bb66fd12b6b
silence717 <yangfang2028@126.com>
2016-12-07 18:01:43 +0800

    scope部分编写

--- src/Scopes/scope.js
+++ src/Scopes/scope.js
@@ -2,8 +2,69 @@
  * @author  https://github.com/silence717
  * @date on 2016/12/7
  */
-export default class Scope {
-	constructor() {
+import _ from 'lodash';
 
+// 引入一个初始化函数
+function initWatchVal() {}
+
+export default class Scope {
+
+	constructor() {
+		// 在Angular框架中，双美元符前缀$$表示这个变量被当作私有的来考虑，不应当在外部代码中调用。
+		// 存储注册过的所有监听器
+		this.$$watchers = [];
 	}
+
+	/**
+	 * 使用$watch，可以在Scope上添加一个监听器。当Scope上发生变更时，监听器会收到提示
+	 * @param watchFn   一个监控函数，用于指定所关注的那部分数据。可以是一个表达式，也可以为一个函数。
+	 * @param listenerFn  一个监听函数，用于在数据变更的时候接受提示。
+	 *
+	 * 为什么添加一个last？
+	 * 比较watch函数返回值和存储last属性的值大部分没有问题。
+	 * 只有第一次执行watch的时候，我们并没有设置last的值，值为undefined。
+	 * 当watch函数首次返回的置为undefined时候，这样的判断就会失效。
+	 * 所以我们添加一个初始化值--空函数，数组也是对象的一直，对象和对象永远不会相等。
+	 */
+	$watch(watchFn, listenerFn) {
+		const watcher = {
+			watchFn: watchFn,
+			listenerFn: listenerFn || function () {},
+			last: initWatchVal
+		};
+		this.$$watchers.push(watcher);
+	}
+
+	/**
+	 * 对每个监听器，我们调用监控函数，把作用域自身当作实参传递进去，然后比较这个返回值和上次返回值，如果不同，就调用监听函数
+	 * @returns {*} 把所有的监听器运行一次，返回一个布尔值，表示是否还有变更
+	 */
+	$$digestOnce() {
+		let dirty;
+		_.forEach(this.$$watchers, watcher => {
+			const newValue = watcher.watchFn(this);
+			// 取出存取的last为旧值
+			const oldValue = watcher.last;
+			if (newValue !== oldValue) {
+				// 第一次的时候添加判断,旧值为initWatchVal时候，将它替换掉
+				watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), this);
+				// 当新值和旧值不相等的时候我们认为数据是不稳定的，所以为脏
+				dirty = true;
+			}
+			// 每次新旧值不相同的时候，将新值存为last，用于下次和新值做比较
+			watcher.last = newValue;
+		});
+		return dirty;
+	}
+
+	/**
+	 * 调用$$digestOnce,当返回值为true的时候我们认为数据是不稳定的，再次执行$digest，直到数据不再变化为止
+	 */
+	$digest() {
+		let dirty;
+		do {
+			dirty = this.$$digestOnce();
+		} while (dirty);
+	}
+
 }
--- src/Scopes/scope_spec.js
+++ src/Scopes/scope_spec.js
@@ -4,10 +4,112 @@
  */
 'use strict';
 import Scope from './scope';
+
 describe('Scope', function () {
-	it('can be constructed and used as an object', function () {
+	// Angular的Scope对象是POJO（简单的JavaScript对象），在它们上面，可以像对其他对象一样添加属性。
+	it('can be constructed and used as an object', () => {
 		const scope = new Scope();
 		scope.aProperty = 1;
 		expect(scope.aProperty).toBe(1);
+	});
+
+	describe('digest', () => {
+		let scope;
+
+		beforeEach(() => {
+			scope = new Scope();
+		});
+		// 测试$digest调用后，监听器函数被调用
+		it('calls the listener function of a watch on first $digest', () => {
+			const watchFn = () => 'wat';
+			const listenerFn = jasmine.createSpy();
+			scope.$watch(watchFn, listenerFn);
+
+			scope.$digest();
+
+			expect(listenerFn).toHaveBeenCalled();
+		});
+		// 在调用监控函数的时候，使用当前作用域作为实参，就是scope作为watchFn的参数
+		it('calls the watch function with the scope as the argument', () => {
+			const watchFn = jasmine.createSpy();
+			const listenerFn = () => { };
+			scope.$watch(watchFn, listenerFn);
+			scope.$digest();
+			expect(watchFn).toHaveBeenCalledWith(scope);
+		});
+		// 当scope监听的值发生改变的时候再去执行listenerFn
+		it('calls the listener function when the watched value changes', () => {
+			scope.someValue = 'a';
+			scope.counter = 0;
+			scope.$watch(
+				scope => scope.someValue,
+				(newValue, oldValue, scope) => { scope.counter++; }
+			);
+			expect(scope.counter).toBe(0);
+			scope.$digest();
+			expect(scope.counter).toBe(1);
+			scope.$digest();
+			expect(scope.counter).toBe(1);
+			scope.someValue = 'b';
+			expect(scope.counter).toBe(1);
+			scope.$digest();
+			expect(scope.counter).toBe(2);
+		});
+		// 当watchFn的的值第一次为undefined时候也需要调用listenerFn
+		// 当没有初始化last值得时候，此测试不能通过
+		it('calls listener when watch value is first undefined', () => {
+			scope.counter = 0;
+			scope.$watch(
+				scope => scope.someValue,
+				(newValue, oldValue, scope) => { scope.counter++; }
+			);
+			scope.$digest();
+			expect(scope.counter).toBe(1);
+		});
+		// 第一次的时候添加判断,旧值为initWatchVal时候，将它替换掉
+		it('calls listener with new value as old value the first time', () => {
+			scope.someValue = 123;
+			let oldValueGiven = '';
+			scope.$watch(
+				scope => scope.someValue,
+				(newValue, oldValue) => { oldValueGiven = oldValue; }
+			);
+			scope.$digest();
+			expect(oldValueGiven).toBe(123);
+		});
+		// 如果你想在每次Angular的作用域被digest的时候得到通知，可以利用每次digest的时候挨个执行监听器这个事情，
+		// 只要注册一个没有监听函数的监听器就可以了。
+		// 想要支持这个用例，我们需要在$watch里面检测是否监控函数被省略了，如果是这样，用个空函数来代替它
+		it('may have watchers that omit the listener function', () => {
+			const watchFn = jasmine.createSpy().and.returnValue('something');
+			scope.$watch(watchFn);
+			scope.$digest();
+			expect(watchFn).toHaveBeenCalled();
+		});
+		// 当数据脏的时候持续Digest
+		it('triggers chained watchers in the same digest', () => {
+			scope.name = 'Jane';
+			// 第一个watch变量scope.nameUpper，第一次执行的时候scope.nameUpper不存在，所以listenerFn不执行。这个时候就需要持续digest，直到数据稳定
+			scope.$watch(
+				scope => scope.nameUpper,
+				(newValue, oldValue, scope) => {
+					if (newValue) {
+						scope.initial = newValue.substring(0, 1) + '.';
+					}
+				});
+			// 第二个watch变量scope.name，第一次执行存在，执行listenerFn,给scope.nameUpper赋值
+			scope.$watch(
+				scope => scope.name,
+				(newValue, oldValue, scope) => {
+					if (newValue) {
+						scope.nameUpper = newValue.toUpperCase();
+					}
+				});
+			scope.$digest();
+			expect(scope.initial).toBe('J.');
+			scope.name = 'Bob';
+			scope.$digest();
+			expect(scope.initial).toBe('B.');
+		});
 	});
 });

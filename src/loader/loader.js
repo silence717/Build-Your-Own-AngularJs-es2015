1ca4eefa274cdddebbbbe156e5f11be21e95ccc8
silence717 <yangfang2028@126.com>
2017-02-08 10:59:56 +0800

    注册和获取一个模块

--- src/loader/loader.js
+++ src/loader/loader.js
@@ -3,8 +3,63 @@
  * @date on 2017/2/7
  */
 export default function setupModuleLoader(window) {
-	const ensure = function(obj, name, factory) {
+	/**
+	 * 为对象添加一个属性，仅仅当这个对象上的属性不存在的时候
+	 * @param obj  对象
+	 * @param name  属性
+	 * @param factory  工厂方法
+	 * @returns {*}
+	 */
+	const ensure = (obj, name, factory) => {
 		return obj[name] || (obj[name] = factory());
 	};
+	// 创建一个全局的angular对象
 	const angular = ensure(window, 'angular', Object);
+	/**
+	 * 创建一个module对象
+	 * @param name  module名称
+	 * @param requires  依赖的其他模块
+	 * @param modules
+	 * @returns {{name: *}}
+	 */
+	const createModule = (name, requires, modules) => {
+		// module名称不能使用hasOwnProperty，因为获取module使用它去做检测
+		if (name === 'hasOwnProperty') {
+			throw 'hasOwnProperty is not a valid module name';
+		}
+		const moduleInstance = {
+			name: name,
+			requires: requires
+		};
+		// 创建modules的时候将其存入到之前的私有modules
+		modules[name] = moduleInstance;
+		return moduleInstance;
+	};
+	/**
+	 * 从当前已存在modules对象获取查找的module
+	 * @param name  需要获取的module名称
+	 * @param modules
+	 * @returns {*}
+	 */
+	const getModule = (name, modules) => {
+		// 使用hasOwnProperty（只是检测自身的属性，不包含继承）先去检测是否存在这个module，不存在的话抛出异常，当前查找module不可用
+		if (modules.hasOwnProperty(name)) {
+			return modules[name];
+		} else {
+			throw 'Module' + name + 'is not available!';
+		}
+	};
+	// 为angular添加一个module方法
+	ensure(angular, 'module', () => {
+		// 存储所有module
+		const modules = {};
+		return (name, requires) => {
+			// 通过requires是否存在来判断是新建module，还是获取module
+			if (requires) {
+				return createModule(name, requires, modules);
+			} else {
+				return getModule(name, modules);
+			}
+		};
+	});
 }
--- src/loader/loader_spec.js
+++ src/loader/loader_spec.js
@@ -21,4 +21,53 @@ describe('setupModuleLoader', () => {
 		setupModuleLoader(window);
 		expect(window.angular).toBe(ng);
 	});
+	it('exposes the angular module function', () => {
+		setupModuleLoader(window);
+		expect(window.angular.module).toBeDefined();
+	});
+	it('exposes the angular module function just once', () => {
+		setupModuleLoader(window);
+		const module = window.angular.module;
+		setupModuleLoader(window);
+		expect(window.angular.module).toBe(module);
+	});
+	describe('modules', () => {
+
+		beforeEach(() => {
+			setupModuleLoader(window);
+		});
+
+		it('allows registering a module', () => {
+			const myModule = window.angular.module('myModule', []);
+			expect(myModule).toBeDefined();
+			expect(myModule.name).toEqual('myModule');
+		});
+
+		it('replaces a module when registered with same name again', () => {
+			const myModule = window.angular.module('myModule', []);
+			const myNewModule = window.angular.module('myModule', []);
+			expect(myNewModule).not.toBe(myModule);
+		});
+
+		it('attaches the requires array to the registered module', () => {
+			const myModule = window.angular.module('myModule', ['myOtherModule']);
+			expect(myModule.requires).toEqual(['myOtherModule']);
+		});
+
+		it('allows getting a module', () => {
+			const myModule = window.angular.module('myModule', []);
+			const gotModule = window.angular.module('myModule');
+
+			expect(gotModule).toBeDefined();
+			expect(gotModule).toBe(myModule);
+		});
+
+		it('throws when trying to get a nonexistent module', () => {
+			expect(() => {
+				window.angular.module('myModule');
+			}).toThrow();
+		});
+
+	});
 });
+
